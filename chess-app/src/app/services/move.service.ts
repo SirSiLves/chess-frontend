f79fb0810d184a8c20278d1b46c5cc35314cbf14
Yves <yves@ruosch.me>
2020-09-08 21:28:28 +0200

    bugfix

--- chess-app/src/app/services/move.service.ts
+++ chess-app/src/app/services/move.service.ts
@@ -18,25 +18,24 @@ export class MoveService {
 
 
   constructor(private httpService: HttpService, private toast: ToastrService, private matrixService: MatrixService) {
-    this.sourceField$ = new BehaviorSubject<any>({ fieldDesignation: []});
-    this.targetField$ = new BehaviorSubject<any>({ fieldDesignation: []});
+    this.sourceField$ = new BehaviorSubject<any>({fieldDesignation: []});
+    this.targetField$ = new BehaviorSubject<any>({fieldDesignation: []});
     this.clickedCount = 0;
   }
 
-  getClickedCount(){
+  getClickedCount() {
     return this.clickedCount;
   }
 
 
-  prepareMove(clickedField){
+  prepareMove(clickedField) {
 
     this.clickedCount++;
     this.onClick.emit(this.clickedCount);
 
-    if(this.clickedCount == 1 && clickedField.figure != null){
+    if (this.clickedCount == 1 && clickedField.figure != null) {
       this.sourceField$.next(clickedField);
-    }
-    else if(this.clickedCount >= 2){
+    } else if (this.clickedCount >= 2) {
       this.targetField$.next(clickedField);
       this.doMove();
     }
@@ -52,18 +51,17 @@ export class MoveService {
   }
 
 
-  doMove(){
+  doMove() {
 
     const moveObj = {
-      sourceField : this.sourceField$.value.fieldDesignation,
-      targetField : this.targetField$.value.fieldDesignation
+      sourceField: this.sourceField$.value.fieldDesignation,
+      targetField: this.targetField$.value.fieldDesignation
     }
 
     this.httpService.validateMove(moveObj).subscribe(validateResponse => {
-      if(validateResponse.state){
+      if (validateResponse.state) {
         this.reloadGamePicture();
-      }
-      else {
+      } else {
         this.toast.warning(validateResponse.text)
         this.printErrorUnitTestsForApi(moveObj);
       }
@@ -72,7 +70,7 @@ export class MoveService {
     this.clickedCount = 0;
   }
 
-  reloadGamePicture(){
+  reloadGamePicture() {
     this.httpService.getGamePicture().subscribe(responsePicture => {
       this.matrixService.setMatrix(responsePicture.board.fieldMatrix);
 
@@ -81,37 +79,35 @@ export class MoveService {
   }
 
 
-  printSuccessUnitTestsForApi(moveHistory){
-    // assertTrue(moveService.handleMove(new String[]{'f', '7'}, new String[]{'f', '5'}).isState());
+  printSuccessUnitTestsForApi(moveHistory) {
+    // assertTrue(handleMoveService.handleMove(new String[]{'f', '7'}, new String[]{'f', '5'}).isState());
 
     const moveHistorySize = Object.keys(moveHistory).length
     let testString = '';
 
-    for(let i = 0; i < moveHistorySize; i++){
-      testString += 'assertTrue(moveService.handleMove(';
+    for (let i = 0; i < moveHistorySize; i++) {
+      testString += 'assertTrue(handleMoveService.handleMove(';
 
-      let sourceField = 'new String[]{"'+ moveHistory[i].sourceField[0] +'", "'+ moveHistory[i].sourceField[1]+'"}';
-      let targetField = 'new String[]{"'+ moveHistory[i].targetField[0] +'", "'+ moveHistory[i].targetField[1]+'"}';
+      let sourceField = 'new String[]{"' + moveHistory[i].sourceField.fieldDesignation[0] + '", "' + moveHistory[i].sourceField.fieldDesignation[1] + '"}';
+      let targetField = 'new String[]{"' + moveHistory[i].targetField.fieldDesignation[0] + '", "' + moveHistory[i].targetField.fieldDesignation[1] + '"}';
 
       testString += sourceField + ', ' + targetField + ').isState());' + '\n';
     }
     console.log(testString);
   }
 
-  printErrorUnitTestsForApi(moveObj){
-    // assertFalse(moveService.handleMove(new String[]{'f', '7'}, new String[]{'f', '5'}).isState());
+  printErrorUnitTestsForApi(moveObj) {
+    // assertFalse(handleMoveService.handleMove(new String[]{'f', '7'}, new String[]{'f', '5'}).isState());
 
-    let testString = 'assertFalse(moveService.handleMove(';
-    let sourceField = 'new String[]{"'+ moveObj.sourceField[0] +'", "'+ moveObj.sourceField[1]+'"}';
-    let targetField = 'new String[]{"'+ moveObj.targetField[0] +'", "'+ moveObj.targetField[1]+'"}';
+    let testString = 'assertFalse(handleMoveService.handleMove(';
+    let sourceField = 'new String[]{"' + moveObj.sourceField[0] + '", "' + moveObj.sourceField[1] + '"}';
+    let targetField = 'new String[]{"' + moveObj.targetField[0] + '", "' + moveObj.targetField[1] + '"}';
 
     testString += sourceField + ', ' + targetField + ').isState());' + '\n';
 
     console.log(testString);
 
   }
-
-
 
 
 }
